2e39a619d72214d70a5c49a179b2b5c7683cc58b
Renata Furini <renatafurini@unochapeco.edu.br>
2019-10-07 20:28:32 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
-'''operadores matematicos'''
-operacao = input('''
+'''Definindo função'''
+
+def calcular():
+    operacao = input('''
     Favor escolher uma das operações abaixo:
     + para Adição
     - para Subtração
@@ -7,25 +9,46 @@ operacao = input('''
     / para Divisão
     ''')
 
-'''solicitando numeros'''
-num_1 = int(input ('informe o primeiro numero: '))
-num_2 = int(input ('informe o segundo numero: '))
+   
+    
+    '''solicitando numeros'''
+    num_1 = int(input ('informe o primeiro numero: '))
+    num_2 = int(input ('informe o segundo numero: '))
 
 
-'''validando o operador selecionado'''
+    '''validando o operador selecionado'''
 
-if operacao == '+':
-    print ('{} + {} = '.format(num_1, num_2),(num_1 + num_2))
+    if operacao == '+':
+        print ('{} + {} = '.format(num_1, num_2),(num_1 + num_2))
 
-elif operacao == '-':
-    print ('{} - {} = '.format(num_1, num_2),(num_1 - num_2))
+    elif operacao == '-':
+        print ('{} - {} = '.format(num_1, num_2),(num_1 - num_2))
 
-elif operacao == '*':
-    print ('{} * {} = '.format(num_1, num_2),(num_1 * num_2))
+    elif operacao == '*':
+        print ('{} * {} = '.format(num_1, num_2),(num_1 * num_2))
 
-elif operacao == '/':
-    print ('{} / {} = '.format(num_1, num_2),(num_1 / num_2))
+    elif operacao == '/':
+        print ('{} / {} = '.format(num_1, num_2),(num_1 / num_2))
 
-else:
-    print ('Operador invalido!!!')
+    else:
+        print ('Operador invalido!!!')
+        
+    calcularNovamente()
+        
+ # Defina a função calcularNovamente () para perguntar ao usuário se ele deseja usar a calculadora novamente
+def calcularNovamente():
+    calc_nova = input ('''
+        Deseja realizar novo calculo?
+        Use S para sim e N para Nao.
+        ''')
+            # se for Y, gera novo calculo
+    if calc_nova == 'S' or calc_nova == 'S':
+        calcular()
+                # se for N, sai da calculadora
+    elif calc_nova == 'N' or calc_nova == 'n':
+        print('Até mais!!')
+                # se não informar nenhuma das alternativas volta para a função 
+    else:
+        calcularNovamente()
 
+calcular()
